35cfa90900e59b802f4d338e7710a6601489738d
ARUN-YERRAM <arunyerram12022005@gmail.com>
2024-12-29 11:21:49 +0530

    Added Feature of Character Motion and Improved CSS in All Pages

---
+++ .history/Mental-Health/src/components/Model_20241229105708.js
@@ -0,0 +1,156 @@
+// import React, { useState } from "react";
+// import { Canvas } from "@react-three/fiber";
+// import Character from "../components/Character";
+// import { OrbitControls } from "@react-three/drei";
+
+// const Model = () => {
+//   const [allAnimation, setAllAnimation] = useState([
+//     "golf",
+//     "idle",
+//     "jump",
+//     "pockets",
+//     "react",
+//     "rope",
+//     "shrug",
+//     "swingdance",
+//     "wave",
+//   ]);
+
+//   const [currentAnimationName, setCurrentAnimationName] = useState("golf");
+
+//   return (
+//     <>
+//     <div style={{ width: "100vw", height: "100vh" }}>
+//       <Canvas>
+//         <OrbitControls />
+//         <ambientLight />
+//         <directionalLight position={[-5, 5, 5]} />
+//         <Character currentAnimationName={currentAnimationName} />
+//       </Canvas>
+//       <div
+//         style={{
+//           position: "absolute",
+//           top: 0,
+//           right: "3rem",
+//           display: "flex",
+//           flexDirection: "column",
+//           justifyContent: "center",
+//           height: "100vh",
+//         }}
+//       >
+//         {allAnimation.map((name, i) => (
+//           <div key={i} style={{ backgroundColor: "green",color:"white" ,margin: "0.5rem", borderRadius: "1rem" }}>
+//             <button
+//               onClick={() => setCurrentAnimationName(name)}
+//               style={{ color:'white',padding: "1rem", width: "100%" }}
+//             >
+//               {name}
+//             </button>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//     </>
+    
+//   );
+// };
+
+// export default Model;
+
+
+
+import React, { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import Character from "./Character";
+import { OrbitControls } from "@react-three/drei";
+
+const Model = () => {
+  const [allAnimation, setAllAnimation] = useState([
+    "golf",
+    "idle",
+    "jump",
+    "pockets",
+    "react",
+    "rope",
+    "shrug",
+    "swingdance",
+    "wave",
+  ]);
+
+  const [currentAnimationName, setCurrentAnimationName] = useState("golf");
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-5, 5, 5]} />
+        <Character
+          glbPath="/stacy.glb"
+          texturePath="/stacy.jpg"
+          currentAnimationName={currentAnimationName}
+          position={[0, -1, 2]}
+          scale={0.01}
+        />
+      </Canvas>
+
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-5, 5, 5]} />
+        <Character
+          glbPath="/stacy.glb"
+          texturePath="/stacy.jpg"
+          currentAnimationName={currentAnimationName}
+          position={[0, -1, 2]}
+          scale={0.01}
+        />
+      </Canvas>
+
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-5, 5, 5]} />
+        <Character
+          glbPath="/stacy.glb"
+          texturePath="/stacy.jpg"
+          currentAnimationName={currentAnimationName}
+          position={[0, -1, 2]}
+          scale={0.01}
+        />
+      </Canvas>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          right: "3rem",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        {allAnimation.map((name, i) => (
+          <div
+            key={i}
+            style={{
+              backgroundColor: "green",
+              color: "white",
+              margin: "0.5rem",
+              borderRadius: "1rem",
+            }}
+          >
+            <button
+              onClick={() => setCurrentAnimationName(name)}
+              style={{ color: "white", padding: "1rem", width: "100%" }}
+            >
+              {name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Model;
---
+++ .history/my-app2/src/components/Model_20241229021327.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDprtWmFp0iEIS5_sCEf9gG8XbW0P2mmBA",
+    authDomain: "login-auth-5543b.firebaseapp.com",
+    projectId: "login-auth-5543b",
+    storageBucket: "login-auth-5543b.firebasestorage.app",
+    messagingSenderId: "261251587986",
+    appId: "1:261251587986:web:e709ce20c740f612452b4b"
+  };
+  
+  
