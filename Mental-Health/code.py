09f11f37ee92864118e58b410af4248c7580ab52
ARUN-YERRAM <arunyerram12022005@gmail.com>
2025-01-12 22:09:02 +0530

    Deploy Commit

--- Mental-Health/code.py
+++ Mental-Health/code.py
@@ -7,7 +7,7 @@ from sklearn.datasets import make_classification
 
 
 X, y = make_classification(n_samples=1000, n_features=20, n_informative=15, 
-                           n_redundant=5, random_state=42)
+            n_redundant=5, random_state=42)
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -38,3 +38,4 @@ print(f"Test Accuracy: {accuracy:.4f}")
 predictions = model.predict(X_test)
 predictions = (predictions > 0.5).astype(int)  # Convert probabilities to binary outputs
 print(f"Sample Predictions: {predictions[:10].flatten()}")
+
